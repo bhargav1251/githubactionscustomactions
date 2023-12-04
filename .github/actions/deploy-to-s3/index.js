4e86f3210907c052b1fef5b2bbd97b418a0c22ea
n1586747 <bhargavsubbarao.tammineedi@libertymutual.com>
2023-12-04 14:23:11 -0500

    main: javascript action adding output

--- .github/actions/deploy-to-s3/index.js
+++ .github/actions/deploy-to-s3/index.js
@@ -1,5 +1,9 @@
-import {info, notice} from '@actions/core'
+import {getInput, info, notice, setOutput} from '@actions/core'
 const run = () => {
+    const bucketName = getInput('bucket');
+    const bucketRegion = getInput('bucket-region');
+    const artifactKey = getInput('artifact-key');
+    setOutput('website-url', "https://")
     info("This is my custom action")
     notice("this is my notice")
 }
