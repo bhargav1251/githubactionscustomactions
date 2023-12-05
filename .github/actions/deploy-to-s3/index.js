86be632082998d98cf12bf2fad7504ea72820b8c
n1586747 <bhargavsubbarao.tammineedi@libertymutual.com>
2023-12-05 08:46:02 -0500

    main: javascript action adding output

--- .github/actions/deploy-to-s3/index.js
+++ .github/actions/deploy-to-s3/index.js
@@ -1,9 +1,13 @@
 import {getInput, info, notice, setOutput} from '@actions/core'
+import {context} from "@actions/github";
+
 const run = () => {
     const bucketName = getInput('bucket');
     const bucketRegion = getInput('bucket-region');
     const artifactKey = getInput('artifact-key');
     setOutput('website-url', "https://")
+    notice('the github context is ', context.repo)
+
     info("This is my custom action")
     notice("this is my notice")
 }
